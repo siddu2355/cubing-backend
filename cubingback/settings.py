acd27096b096747e948ca99604f31852a35eee80
siddu2355 <siddusirasanambeti@gmail.com>
2024-04-09 14:30:24 +0530

    last

--- cubingback/settings.py
+++ cubingback/settings.py
@@ -98,29 +98,15 @@ WSGI_APPLICATION = 'cubingback.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cubing',
-#         'HOST':'localhost',
-#         'USER':'root',
-#         'PASSWORD':'#$iddu2355'
-#     }
-# }
-
 DATABASES = {
-    "default":dj_database_url.parse("postgres://srm_cubing_0xxe_user:2xTYOg5fDWqn7PeDRrVauNckIqye5kvo@dpg-ckjanfa12bvs7394ir4g-a.singapore-postgres.render.com/srm_cubing_0xxe")
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cubing',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'#$iddu2355'
+    }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'srm-cubing',
-#         'USER': 'siddu2355',
-#         'PASSWORD': '#Siddu2355',
-#         'HOST': '*',
-#         'PORT': '5432',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
